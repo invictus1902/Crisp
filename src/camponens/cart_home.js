411987dfad600d2687ebe2f096d25d08db1b1290
invictus1902 <131914647+invictus1902@users.noreply.github.com>
2024-02-13 10:22:39 +0600

    первая фиксация

---
+++ src/camponens/cart_home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './cart.scss'
+
+const Cart = () => {
+    return (
+        <div className='cart'>
+             
+        </div>
+    );
+};
+
+export default Cart;
